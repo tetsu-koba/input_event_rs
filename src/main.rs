fc12a01718c85d765eee36cb5321b35077bccc76
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2023-08-20 16:56:56 +0900

    cosmetic

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@
 mod input;
 use crate::input::*;
 
+use std::fs::File;
 use std::io;
 use std::io::Read;
-use std::fs::File;
 
 fn read_input_event(f: &mut File) -> io::Result<input_event> {
     let mut buf = [0; std::mem::size_of::<input_event>()];
@@ -18,26 +18,26 @@ fn dump_input_event(fname: &str) {
     let mut f = File::open(fname).expect("open failed.");
     loop {
         match read_input_event(&mut f).expect("read error.") {
-            input_event { type_, code, value, .. } if type_ as u32 == EV_KEY => {
-                match code as u32 {
-                    KEY_MUTE => {
-                        if value == 1 {
-                            println!("mute")
-                        }
+            input_event {
+                type_, code, value, ..
+            } if type_ as u32 == EV_KEY => match code as u32 {
+                KEY_MUTE => {
+                    if value == 1 {
+                        println!("mute")
                     }
-                    KEY_VOLUMEDOWN => {
-                        if value != 0 {
-                            println!("volume down")
-                        }
-                    }
-                    KEY_VOLUMEUP => {
-                        if value != 0 {
-                            println!("volume up")
-                        }
-                    }
-                    _ => {}
                 }
-            }
+                KEY_VOLUMEDOWN => {
+                    if value != 0 {
+                        println!("volume down")
+                    }
+                }
+                KEY_VOLUMEUP => {
+                    if value != 0 {
+                        println!("volume up")
+                    }
+                }
+                _ => {}
+            },
             //ie => println!("input_event ={:?}", ie),
             _ => {}
         }
